54428e610c04c8969c3f3fad7f5b7f9c451fc463
Steve Lévesque <42849270+steve-levesque@users.noreply.github.com>
2020-12-06 16:11:27 -0500

    Initial Commit
    - Initial commit with FPS Tutorial Game files.
    - Unity .gitignore.
    - Added mobile interface for Android.

---
+++ Assets/Joystick Pack/Scripts/Joysticks/FixedJoystick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedJoystick : Joystick
+{
+    void Update()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
---
+++ Assets/MobileController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileController : MonoBehaviour
+{
+    public FixedButton JumpButton;
+    public FixedButton ShootButton;
+    public FixedJoystick MoveJoystick;
+    public FixedTouchField TouchField;
+
+    public FixedButton Weapon1;
+    public FixedButton Weapon2;
+    public FixedButton Weapon3;
+
+    int counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var fps = GetComponent<PlayerCharacterController>();
+        var wfps = GetComponent<PlayerWeaponsManager>();
+
+        fps.JumpButtonPressed = JumpButton.Pressed;
+        fps.RunAxis = MoveJoystick.Direction;
+        fps.LookAxis = TouchField.TouchDist;
+
+        wfps.ShootButtonPressed = ShootButton.Pressed;
+
+
+        if (Weapon1.Pressed)
+            wfps.MobileWeaponChosen = 1;
+        if (Weapon2.Pressed)
+            wfps.MobileWeaponChosen = 2;
+        if (Weapon3.Pressed)
+            wfps.MobileWeaponChosen = 3;
+    }
+}
